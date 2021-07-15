299667d57588e86d31067f6c612bbbaef40a1e14
SodiumN1trate <renarsgausins21@gmail.com>
2021-07-15 22:34:59 +0300

    Delete system done. Added some new features. Help button started

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -35,7 +35,7 @@ function close_all_drop_downs() {
 
 // Title managment
 let titles = JSON.parse(localStorage.getItem("titles")) || [];
-
+let id = 0;
 render();
 
 function render(){
@@ -46,11 +46,12 @@ function render(){
     expenses_titles.innerHTML = "";
     didnt_buy_it.innerHTML = "";
     for (let index = 0; index < titles.length; index++) {
-        let title = `<div class="drop-down-title">
-                        <span style="display: none;">${ titles[index].id }</span>
+        let title = `<div class="drop-down-title" id="${ titles[index].id }">
                         <span class="drop-down-title-name">${ titles[index].title_name }</span>
-                        <span class="drop-down-title-price">${ titles[index].title_price }€</span>
-                        <span class="delete-title-button">Dzēst</span>
+                        <div>
+                            <span class="drop-down-title-price">${ titles[index].title_price }€</span>
+                            <span class="delete-title-button"><img src="static/vectors/archive.svg" width="20px"></span>
+                        </div>
                     </div>
             `;
         if(titles[index].title_category == "Ienākumi"){
@@ -65,8 +66,8 @@ function render(){
     };
 
     let input = `<div class="drop-down-title-input">
-        <input class="drop-down-title-name-input" placeholder="Nosaukums"  maxlength="20">
-        <input class="drop-down-title-price-input" placeholder="Summa" maxlength="8">
+        <input type="text" class="drop-down-title-name-input" placeholder="Nosaukums"  maxlength="15">
+        <input type="text" class="drop-down-title-price-input" placeholder="Summa" maxlength="6">
         <img src="static/vectors/plus.svg" width="25px">
     </div>`;
     income_titles.innerHTML += input;
@@ -76,6 +77,7 @@ function render(){
     render_balance();
     render_inputs();
     render_delete_buttons();
+    title_sum_render();
 };
 
 
@@ -87,12 +89,13 @@ function render_inputs(){
             if(title_name == "" || title_price == ""){
                 alert("Aizpildiet visus laukus!");
             }
-            else if(title_name.length > 20 || title_price.length > 8){
+            else if(title_name.length > 15 || title_price.length > 6 || parseInt(title_price) <= 0 || parseInt(title_price) == NaN){
                 alert("Notikusi kļūda!");
             }
             else{
-                titles.push({"id": index, "title_name": title_name, "title_price": title_price, "title_category":e.target.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].innerHTML});
+                titles.push({"id": id, "title_name": title_name, "title_price": parseInt(title_price), "title_category":e.target.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].innerHTML});
                 localStorage.setItem("titles", JSON.stringify(titles));
+                id += 1;
                 render();
             };
         });   
@@ -104,7 +107,6 @@ function render_balance(){
     for (let index = 0; index < titles.length; index++) {
         if(titles[index].title_category == "Ienākumi"){
             balance += parseInt(titles[index].title_price);
-            console.log(balance);
         };
         if(titles[index].title_category == "Izdevumi"){
             balance -= parseInt(titles[index].title_price);
@@ -116,12 +118,41 @@ function render_balance(){
 function render_delete_buttons() {
     for(let index = 0; index < document.querySelectorAll(".delete-title-button").length; index++) {
         document.querySelectorAll(".delete-title-button")[index].addEventListener("click", (e)=>{
-            console.log(e.target.parentNode.childNodes[1].innerHTML);
-            titles = titles.splice(e.target.parentNode.childNodes[1].innerHTML, 1);
-            console.log(titles);
+            for (let index = 0; index < titles.length; index++) {
+                if(titles[index].id == e.target.parentNode.parentNode.parentNode.id){
+                    titles.splice(titles.indexOf(titles[index]), 1);
+                };
+            };
             localStorage.setItem("titles", JSON.stringify(titles));
             e.target.parentNode.remove();
             render();
         });
     };
 };
+
+function title_sum_render() {
+    income_sum = 0;
+    expenses_sum = 0;
+    didnt_buy_it_sum = 0;
+    console.log(document.querySelectorAll(".sum")[0]);
+    for (let index = 0; index < titles.length; index++) {
+        if(titles[index].title_category == "Ienākumi"){
+            income_sum += titles[index].title_price;
+        };
+        if(titles[index].title_category == "Izdevumi"){
+            expenses_sum += titles[index].title_price;
+        };
+        if(titles[index].title_category == "Nenopirku"){
+            didnt_buy_it_sum += titles[index].title_price;
+        };
+    };
+    document.querySelectorAll(".sum")[0].innerHTML = income_sum;
+    document.querySelectorAll(".sum")[1].innerHTML = expenses_sum;
+    document.querySelectorAll(".sum")[2].innerHTML = didnt_buy_it_sum;
+};
+
+
+// Help button
+document.querySelector("#footer-content > button").addEventListener("click", (e) =>{
+    alert("Poga rabotaet");
+});
